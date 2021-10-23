e4b9a2ab45f4a2e231ad4aded75a20ecd8ed0749
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-10-23 18:20:53 -0500

    incorporated bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
 
-const App = () => <PortfolioContainer />;
+const App = () => (
+  <div>
+    <h1 className="light-txt">Michael J Hodges</h1>
+    <PortfolioContainer />;
+  </div>
+);
 
 export default App;
