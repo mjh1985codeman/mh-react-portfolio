54c973dc11e5cd821f7733934168184888b8e1b9
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-10-23 20:16:01 -0500

    added picture to the About Component

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,16 +1,25 @@
 import React from "react";
+import famImage from "../../assets/header/fam-pic.jpg";
 
 function About() {
   return (
     <section className="my-5 background-img">
       <h1 id="about">Who am I?</h1>
       <div className="my-2">
+        <a href="https://github.com/mjh1985codeman">
+          <img
+            src={famImage}
+            alt="Picture of Michael Hodges w/ his family at the beach"
+          />
+        </a>
         <p>
-          Husband, Dad, Novice Developer and Working Professional. I have 10
-          Years of payment industry experience which includes client services,
-          management, reconciliation, pos support and navigating 3rd party
-          integration. Currently working my way through a coding boot camp to
-          break into the development community.
+          Hey there! Thanks for checking out my portfolio. My name is Michael
+          Hodges; here a few things about me. I am a Husband, Dad, Novice
+          Developer and Working Professional. I have 10 Years of payment
+          industry experience which includes client services, management,
+          reconciliation, pos support and navigating 3rd party integration.
+          Currently working my way through a coding boot camp to break into the
+          development community.
         </p>
       </div>
     </section>
