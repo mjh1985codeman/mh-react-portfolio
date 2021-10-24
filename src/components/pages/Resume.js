49567cbba6fc2c8289c5b79089bc4f5045827081
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-10-24 08:26:38 -0500

    Made my resume downloadable on the Resume Component

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,13 +1,18 @@
 import React from "react";
+import mhResume from "../../assets/resume/MHResume.docx";
 
 function Resume() {
   return (
-    <section className="my-5 background-img">
-      <h1 id="about">Need to figure out how to DL Resume</h1>
-      <div className="my-2">
-        <p>Resume.</p>
-      </div>
-    </section>
+    <div className="container">
+      <section className="my-5 background-img">
+        <h1 id="about">Thank You!</h1>
+        <div className="my-2">
+          <a href={mhResume} download="MHResume">
+            <button type="button">Click Here to Download My Resume!</button>
+          </a>
+        </div>
+      </section>
+    </div>
   );
 }
 
