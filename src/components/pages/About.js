dfcbff2e321413941c811c6e58930dad01412933
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-10-24 13:48:26 -0500

    deploying site

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,23 +4,26 @@ import famImage from "../../assets/header/fam-pic.jpg";
 function About() {
   return (
     <section className="my-5 background-img">
-      <h1 id="about">Who am I?</h1>
-      <div className="my-2">
-        <a href="https://github.com/mjh1985codeman">
-          <img
-            src={famImage}
-            alt="Michael Hodges with his family at the beach"
-          />
-        </a>
-        <p>
-          Hey there! Thanks for checking out my portfolio. My name is Michael
-          Hodges; here a few things about me. I am a Husband, Dad, Novice
-          Developer and Working Professional. I have 10 Years of payment
-          industry experience which includes client services, management,
-          reconciliation, pos support and navigating 3rd party integration.
-          Currently working my way through a coding boot camp to break into the
-          development community.
-        </p>
+      <div className="container">
+        <h1 id="about">Who am I?</h1>
+        <div className="row justify-content-center">
+          <a href="https://github.com/mjh1985codeman">
+            <img
+              className="img-fluid"
+              src={famImage}
+              alt="Michael Hodges with his family at the beach"
+            />
+          </a>
+          <p className="m-4">
+            Hey there! Thanks for checking out my portfolio. My name is Michael
+            Hodges; here a few things about me. I am a Husband, Dad, Novice
+            Developer and Working Professional. I have 10 Years of payment
+            industry experience which includes client services, management,
+            reconciliation, pos support and navigating 3rd party integration.
+            Currently working my way through a coding boot camp to break into
+            the development community.
+          </p>
+        </div>
       </div>
     </section>
   );
